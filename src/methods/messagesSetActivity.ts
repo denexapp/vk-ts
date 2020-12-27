116bbbcc0067bd56e5ec31b46be7c43113a446cb
Denis Mukhametov <denis.mukhametov@gmail.com>
2020-12-27 21:37:44 +0500

    Add messages.setActivity method

---
+++ src/methods/messagesSetActivity.ts
@@ -0,0 +1,23 @@
+import { JsonDecoder } from 'ts.data.json'
+import VK from '..'
+import makeVkRequest from '../utils/makeVkRequest'
+
+export type MessagesActivityType = 'typing' | 'audiomessage'
+
+export type MessagesSetActivityResponse = 1
+
+const messageSetActivityDecoder: JsonDecoder.Decoder<MessagesSetActivityResponse> = JsonDecoder.isExactly(1)
+
+const messagesSetActivity = async (vk: VK, peerId: number, type: MessagesActivityType): Promise<MessagesSetActivityResponse> => {
+  return await makeVkRequest(
+    'mmessages.setActivity',
+    vk.accessToken,
+    messageSetActivityDecoder,
+    {
+      type,
+      peer_id: peerId,
+    }
+  )
+}
+
+export default messagesSetActivity
