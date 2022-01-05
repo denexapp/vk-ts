02527fc8e545d049727f924afbdac87b02f3be74
Denis Mukhametov <mukhametov.d@skbkontur.ru>
2022-01-05 11:11:09 +0300

    Delete tests

--- test/index.test.ts
+++
@@ -1,8 +0,0 @@
-import VK from '../src/index'
-
-describe('VK', () => {
-  it('keeps access token', () => {
-    const vk = new VK('test-token')
-    expect(vk.accessToken).toEqual('test-token')
-  })
-})
