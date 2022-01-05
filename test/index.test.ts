a12c56117b47b59b3dd7ecf79963a1a7a3099833
Denis Mukhametov <mukhametov.d@skbkontur.ru>
2022-01-05 10:51:47 +0300

    Switch to dts-cli

---
+++ test/index.test.ts
@@ -0,0 +1,8 @@
+import VK from '../src/index';
+
+describe('VK', () => {
+  it('keeps access token', () => {
+    const vk = new VK('test-token');
+    expect(vk.accessToken).toEqual('test-token');
+  });
+});
